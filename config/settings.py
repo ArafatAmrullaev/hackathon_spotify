e642937239f8b6b8c6b52b30b383e6374565afb4
Arafat <arafatamrullaev021@gmail.com>
2022-08-17 12:10:53 +0600

    procfile

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-n!xfctm6ja25@fi5i_6n@#z#h=2d^-3!7d6vkl@^(xatr0ah-z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
